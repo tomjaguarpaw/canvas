7ab9b303951bd1d86db73239e5410be1cc1490fe
Tom Ellis <tom-2013@purelyagile.com>
2015-01-15 13:51:46 +0000

    Long lived sockets

--- websocket.hs
+++ websocket.hs
@@ -6,21 +6,25 @@ import qualified Network.WebSockets as WS
 import           Data.Monoid        ((<>))
 import qualified Data.IORef         as R
 
-svg :: T.Text -> T.Text
-svg m = "<svg width='100' height='100'>\
-\  <circle id='bar' cx='50' cy='50' r='40' stroke='green' stroke-width='4' fill='yellow' \
+svg :: Bool -> T.Text -> T.Text
+svg over m = "<svg width='100' height='100'>\
+\  <circle id='bar' cx='50' cy='50' r='40' stroke='green' stroke-width='4' fill='" <> colour <> "' \
 \          onmouseover='mouseover(this.id)'\
 \          onmouseout='mouseout(this.id)'\
 \/>\
 \  <p>" <> m <> "</p>"
+    where colour = if over then "yellow" else "red"
+
 
 meow :: R.IORef Int -> WS.PendingConnection -> IO ()
-meow r pc = forever $ do
+meow r pc = do
   conn <- WS.acceptRequest pc
-  msg  <- WS.receiveData conn
-  n    <- R.readIORef r
-  R.writeIORef r (n + 1)
-  WS.sendTextData conn (svg (msg <> T.pack (show n)))
+  forever $ do
+    msg  <- WS.receiveData conn
+    n    <- R.readIORef r
+    let over = msg == "over"
+    R.writeIORef r (n + 1)
+    WS.sendTextData conn (svg over (msg <> T.pack (show n)))
 
 main :: IO ()
 main = do
