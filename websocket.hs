776321a9e534ea5788bc8d0de7bb2e94c4f179de
Tom Ellis <tom-2013@purelyagile.com>
2015-01-15 13:52:49 +0000

    Don't display message

--- websocket.hs
+++ websocket.hs
@@ -6,13 +6,12 @@ import qualified Network.WebSockets as WS
 import           Data.Monoid        ((<>))
 import qualified Data.IORef         as R
 
-svg :: Bool -> T.Text -> T.Text
-svg over m = "<svg width='100' height='100'>\
+svg :: Bool -> T.Text
+svg over = "<svg width='100' height='100'>\
 \  <circle id='bar' cx='50' cy='50' r='40' stroke='green' stroke-width='4' fill='" <> colour <> "' \
 \          onmouseover='mouseover(this.id)'\
 \          onmouseout='mouseout(this.id)'\
-\/>\
-\  <p>" <> m <> "</p>"
+\/>"
     where colour = if over then "yellow" else "red"
 
 
@@ -22,9 +21,9 @@ meow r pc = do
   forever $ do
     msg  <- WS.receiveData conn
     n    <- R.readIORef r
-    let over = msg == "over"
+    let over = msg == ("over" :: T.Text)
     R.writeIORef r (n + 1)
-    WS.sendTextData conn (svg over (msg <> T.pack (show n)))
+    WS.sendTextData conn (svg over)
 
 main :: IO ()
 main = do
