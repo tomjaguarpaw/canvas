5ce86c95f708699acd41617d6490351861ff099f
Tom Ellis <tom-2013@purelyagile.com>
2015-01-22 16:10:20 +0000

    elementHtml

--- Doc.hs
+++ Doc.hs
@@ -7,6 +7,8 @@ import qualified Data.Text.Lazy     as T
 import           Data.Monoid        ((<>))
 import qualified Text.Blaze         as B
 import           Text.Blaze.Html5   ((!))
+import qualified Text.Blaze.Html5   as H
+import qualified Text.Blaze.Html5.Attributes as AH
 import qualified Text.Blaze.Svg11   as S
 import qualified Text.Blaze.Svg11.Attributes as AS
 import           Text.Blaze.Html.Renderer.Text (renderHtml)
@@ -14,6 +16,9 @@ import           Text.Blaze.Html.Renderer.Text (renderHtml)
 type Message = T.Text
 
 data Doc d a = Doc d (Message -> Maybe a)
+
+data Element = GUICircles [GUICircle]
+             | Button T.Text
 
 instance Functor (Doc d) where
   fmap f (Doc cs h) = Doc cs ((fmap . fmap) f h)
@@ -33,15 +38,18 @@ horiz (Doc xs xh) (Doc ys yh) = Doc (xs ++ ys)
                                         Nothing -> case yh message of
                                           s@(Just _) -> s
                                           Nothing -> Nothing)
+
 handleMessage :: Canvas a -> Message -> Maybe a
 handleMessage (Doc _ h) = h
 
-render :: Canvas a -> T.Text
-render (Doc cs _) = renderHtml (documentSvg h w (sequence_ (package cs [0..])))
-
-  where package = zipWith (\c i -> circleSvg (50 + i * 100) 50 (B.toValue (gcColor c)) (B.toValue (gcName c)))
+circlesSvg :: [GUICircle] -> S.Svg
+circlesSvg cs = (documentSvg h w . sequence_ . package [0..]) cs
+  where package = zipWith (\i c -> circleSvg (50 + i * 100) 50 (B.toValue (gcColor c)) (B.toValue (gcName c)))
         w = 100 * length cs
         h = 100
+
+render :: Canvas a -> T.Text
+render (Doc cs _) = renderHtml (circlesSvg cs)
 
 documentSvg :: Int -> Int -> S.Svg -> S.Svg
 documentSvg h w = S.svg ! AS.width (B.toValue w)
@@ -59,3 +67,10 @@ circleSvg cx cy color name =
            ! AS.onmouseout  (handler "mouseout")
            ! AS.onclick     (handler "click")
     where handler h = h <> "('" <> name <> "')"
+
+buttonHtml :: T.Text -> H.Html
+buttonHtml = (H.button ! AH.type_ "button") . H.toHtml
+
+elementHtml :: Element -> H.Html
+elementHtml = \case GUICircles gs -> circlesSvg gs
+                    Button t      -> buttonHtml t
