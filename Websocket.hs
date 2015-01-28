5544905037b313d89635d734007d8be95db06b8f
Tom Ellis <tom-2013@purelyagile.com>
2015-01-28 22:12:33 +0000

    Generalize radioA

--- Websocket.hs
+++ Websocket.hs
@@ -111,21 +111,21 @@ radioW cx co = R.traverseRadio (A.liftA2 (,))
               in tupleOfResponse (handler co (behaviourO ev radioOOld radioONew)))
                                       (widget co (R.focusedO radioOOld))
 
-radioA :: Widget d1 e x -> Widget d2 e o
-       -> Radio (x, a) (o, a)
-       -> D.DocF ((e, a), Radio x o) (Radio d1 d2)
+radioA :: Widget d1 e1 x -> Widget d2 e2 o
+       -> Radio (x, a1) (o, a2)
+       -> D.DocF (Either (e1, a1) (e2, a2), Radio x o) (Radio d1 d2)
 radioA wx wo = R.traverseRadio (A.liftA2 (,))
                . extendRadio fx fo
   where fx radioXAOld = D.fmapResponse (\(ev, (xNew, a)) ->
               let radioXOld = R.fmapRadioX fst fst radioXAOld
                   radioXNew = R.setFocusedX xNew radioXOld
-              in ((ev, a), R.stampX radioXNew))
+              in (Left (ev, a), R.stampX radioXNew))
                         (let (xOld, a) = R.focusedX radioXAOld
                          in D.fmapResponse ((L.over L._2) (\xNew -> (xNew, a))) (wx xOld))
         fo radioOAOld = D.fmapResponse (\(ev, (oNew, a)) ->
               let radioOOld = R.fmapRadioO fst fst radioOAOld
                   radioONew = R.setFocusedO oNew radioOOld
-              in ((ev, a), R.stampO radioONew))
+              in (Right (ev, a), R.stampO radioONew))
                         (let (oOld, a) = R.focusedO radioOAOld
                          in D.fmapResponse ((L.over L._2) (\xNew -> (xNew, a))) (wo oOld))
 
