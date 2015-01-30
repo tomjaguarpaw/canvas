01bb51b5526dd4913dde6cebcd50039ae6ed1bcf
Tom Ellis <tom-2013@purelyagile.com>
2015-01-30 19:28:20 +0000

    Select works

--- Doc3.hs
+++ Doc3.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase #-}
 
 module Doc3 where
 
@@ -6,8 +7,10 @@ import Doc                          (US)
 import qualified Control.Lens       as L
 import qualified Control.Applicative as A
 import qualified TextEntry          as T
+import qualified Select             as S
 import qualified Doc                as D
 import qualified Network.WebSockets as WS
+import qualified Data.List.NonEmpty as NEL
 
 data DocP f b d = DocP (US (f b, d))
 
@@ -19,6 +22,9 @@ mapDocP f (DocP u) = DocP (L.over (L.mapped.L._2) f u)
 mapResponseP :: Functor f => (b -> b') -> DocP f b d -> DocP f b' d
 mapResponseP f (DocP u) = DocP (L.over (L.mapped.L._1.L.mapped) f u)
 
+mapEvent :: (e -> e') -> Doc e b d -> Doc e' b d
+mapEvent f (Doc (DocP us)) = Doc (DocP (L.over (L.mapped.L._1.answer.attached) f us))
+
 pairP :: A.Applicative f => DocP f b d -> DocP f b' d' -> DocP f (b, b') (d, d')
 pairP (DocP u) (DocP u') = DocP (A.liftA2 pair' u u')
   where pair' (fb, d) (fb', d') = (A.liftA2 (,) fb fb', (d, d'))
@@ -26,10 +32,21 @@ pairP (DocP u) (DocP u') = DocP (A.liftA2 pair' u u')
 pair :: Doc a b d -> Doc a b' d' -> Doc a (b, b') (d, d')
 pair (Doc dp) (Doc dp') = Doc (pairP dp dp')
 
+pairC :: (b -> Doc a b d) -> (b' -> Doc a b' d')
+      -> ((b, b') -> Doc a (b, b') (d, d'))
+pairC w1 w2 (b1, b2) = w1 b1 `pair` w2 b2
+
+pairE :: (b -> Doc a b d) -> (b' -> Doc a' b' d')
+      -> ((b, b') -> Doc (Either a a') (b, b') (d, d'))
+pairE w1 w2 (b1, b2) = mapEvent Left (w1 b1) `pair` mapEvent Right (w2 b2)
+
 mapDoc :: (d -> d') -> Doc a b d -> Doc a b d'
 mapDoc f (Doc dp) = Doc (mapDocP f dp)
 
 data ReadMessage f a = ReadMessage (D.Message -> f a)
+
+answer :: L.Setter (ReadMessage f a) (ReadMessage g b) (f a) (g b)
+answer = L.sets (\f (ReadMessage g) -> ReadMessage (f . g))
 
 instance Functor f => Functor (ReadMessage f) where
   fmap f (ReadMessage fr) = ReadMessage ((fmap.fmap) f fr)
@@ -42,6 +59,12 @@ data Focus a b = NeedFocus a b
                | Don'tNeedFocus a b
                | WantFocus b b
                | Don'tWantFocus b
+
+attached :: L.Traversal (Focus a b) (Focus a' b) a a'
+attached f = \case NeedFocus a b -> fmap (\a' -> NeedFocus a' b) (f a)
+                   Don'tNeedFocus a b -> fmap (\a' -> Don'tNeedFocus a' b) (f a)
+                   WantFocus b b' -> A.pure (WantFocus b b')
+                   Don'tWantFocus b -> A.pure (Don'tWantFocus b)
 
 instance Functor (Focus a) where
   fmap f (NeedFocus a b) = NeedFocus a (f b)
@@ -89,6 +112,20 @@ textEntryC te = (Doc
                  . D.unDoc
                  . T.textEntry) te
 
+
+selectC :: S.Select a -> Doc (S.SelectEvent a) (S.Select a) [D.Element]
+selectC se = (Doc
+                 . DocP
+                 . fmap (\(d, m) ->
+                          (ReadMessage (\message -> case m message of
+                                           Nothing -> Don'tWantFocus se
+                                           Just seev -> Don'tNeedFocus
+                                                        seev
+                                                        (S.selectHandle seev se)
+                                       ), d))
+                 . D.unDoc
+                 . S.select) se
+
 two :: (T.TextEntry, T.TextEntry)
     -> Doc T.TextEntryEvent (T.TextEntry, T.TextEntry) [D.Element]
 two (t1, t2) = mapDoc (uncurry (++))
@@ -116,9 +153,10 @@ runServer :: WS.PendingConnection -> IO ()
 runServer pc = do
   conn <- WS.acceptRequest pc
 
-  let initialGui = (T.textEntryMake "foo", T.textEntryMake "bar")
+  let initialGui = ( (T.textEntryMake "foo", T.textEntryMake "bar")
+                   , (S.selectMake ("foo" NEL.:| ["bar", "baz"])))
 
-      gui = two
+      gui = mapDoc (uncurry (++)) . (two `pairE` selectC)
 
       handler d = do
         WS.sendTextData conn (D.renderElements' d)
