03ecd1a55d3eb26cd69aa1dabba32b4a6fc032c2
Tom Ellis <tom-2013@purelyagile.com>
2015-01-31 16:53:18 +0000

    Remove some unused code

--- Filter.hs
+++ Filter.hs
@@ -5,14 +5,9 @@ module Filter where
 
 import qualified TextEntry          as T
 import qualified Select             as S
-import qualified TextSelect         as TS
 import qualified Data.Text.Lazy     as DT
 import qualified Radio              as R
-import qualified Doc                as D
-import qualified Widget             as W
 import qualified Control.Lens       as L
-import qualified Focused            as F
-import qualified Control.Applicative as A
 
 data Void = Void !Void
 
@@ -29,32 +24,10 @@ data Filter = Filter { _fAv :: Available
               deriving Show
 $(L.makeLenses ''Filter)
 
-
 data FilterEvent = FilterEvent T.TextEntryEvent
                  | EditorEvent T.TextEntryEvent
                  | SelectEvent (S.SelectEvent Int)
 $(L.makePrisms ''FilterEvent)
-
-filterB :: F.Focused Filter -> D.DocF (FilterEvent, F.Focused Filter) [D.Element]
-filterB = eventMatches' _EditorEvent
-      (\_ a -> L.set (F.contentsS.fAv.aAv.R.chosen)
-                     (L.view (F.contents.fEd.T.tText) a) a)
-          . eventMatches' _FilterEvent
-      (\_ a -> L.set (F.contentsS.fSe)
-                   (selectFromAvailable (L.view (F.contents.fFi) a)
-                                        (L.view (F.contents.fAv) a)) a)
-          . eventMatches' (_SelectEvent.S.cEv)
-      (\i a -> L.over (F.contentsS.fAv.aAv) (R.chooseIndex i) a)
-          . filterA
-
-eventMatches' l f = eventMatches l (\i (ev, a) -> (ev, f i a))
-
-eventMatches l f = D.fmapResponse (perhaps (\(ev, a) ->
-  ev L.^? l L.<&> (\ev' -> f ev' (ev, a))))
-
-perhaps :: (a -> Maybe a) -> a -> a
-perhaps f a = case f a of Nothing -> a
-                          Just a' -> a'
 
 selectFromAvailable :: T.TextEntry -> Available -> S.Select Int
 selectFromAvailable t = flip S.Select False
@@ -63,30 +36,6 @@ selectFromAvailable t = flip S.Select False
                         . R.getEnumerate
                         . R.traverseRadio' R.enumerate R.enumerate
                         . L.view aAv
-
-filterA :: F.Focused Filter -> D.DocF (FilterEvent, F.Focused Filter) [D.Element]
-filterA ff = D.fmapResponse (L.over L._1
-                                 (either (either absurd FilterEvent)
-                                         (either EditorEvent SelectEvent ))) $
-                            D.fmapNewState (fmap (\((a', t'), (tt', s')) ->
-                                                          Filter a' t' tt' s')) $
-                            fmap (\(((), e1), e2) -> e1 ++ e2) $
-                            (pureF D.static
-                             `pairF` T.textEntryC
-                             `pairF` TS.textSelectF)
-                            (fmap (\(Filter a t tt s) -> ((a, t), (tt, s))) ff)
-
-pureF :: W.Widget d ev x -> W.Widget d ev (F.Focused x)
-pureF f = D.fmapNewState A.pure . f . F.mostFocused
-
-pairF :: W.Widget d1 ev1 (F.Focused x1)
-      -> W.Widget d2 ev2 (F.Focused x2)
-      -> W.Widget (d1, d2) (Either ev1 ev2) (F.Focused (x1, x2))
-pairF w1 w2 t = (D.fmapResponse (\(ev, (fl, fr)) ->
-                               case ev of
-                                 Left _ -> (ev, A.liftA2 (,) fl fr)
-                                 Right _ -> (ev, A.liftA2 (flip (,)) fr fl)))
-                (W.pair w1 w2 (fmap fst t, fmap snd t))
 
 filterMake :: Filter
 filterMake = Filter available
