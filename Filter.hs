5b69486689a0b47c3d588a1922d2abcc2538b30a
Tom Ellis <tom-2013@purelyagile.com>
2015-02-20 18:13:41 +0000

    Neaten lenses

--- Filter.hs
+++ Filter.hs
@@ -13,12 +13,9 @@ import qualified Html               as H
 import           Doc3               (Doc, emitting, contains, also,
                                      handle, absurd, static)
 
-data Available = Available { _aAv :: R.Radio DT.Text DT.Text } deriving Show
-$(L.makeLenses ''Available)
-
-data Filter = Filter { _fAv :: Available
-                     , _fFi :: T.TextEntry
-                     , _fTS :: (T.TextEntry, S.Select Int) }
+data Filter = Filter { _fAvailable  :: R.Radio DT.Text DT.Text
+                     , _fFilter     :: T.TextEntry
+                     , _fTextSelect :: (T.TextEntry, S.Select Int) }
               deriving Show
 $(L.makeLenses ''Filter)
 
@@ -27,13 +24,12 @@ data FilterEvent = FilterEvent T.TextEntryEvent
                  | SelectEvent (S.SelectEvent Int)
 $(L.makePrisms ''FilterEvent)
 
-selectFromAvailable :: T.TextEntry -> Available -> S.Select Int
+selectFromAvailable :: T.TextEntry -> R.Radio DT.Text DT.Text -> S.Select Int
 selectFromAvailable t = flip S.Select False
                         . R.filterRadio (\(x, _) ->
                                           L.view T.tText t `DT.isPrefixOf` x)
                         . R.getEnumerate
                         . R.traverseRadio R.enumerate R.enumerate
-                        . L.view aAv
 
 filterMake :: Filter
 filterMake = Filter available
@@ -41,21 +37,20 @@ filterMake = Filter available
                     (T.textEntryMake (L.view (S.sRadio.R.chosen.L._1) select),
                     select)
   where select = selectFromAvailable textEntry available
-        available = Available (R.Chosen "tom 1" ["tom 2", "tim 1", "tim 2",
-                                                 "bob 1", "bob 2"])
+        available = R.Chosen "tom 1" ["tom 2", "tim 1", "tim 2", "bob 1", "bob 2"]
         textEntry = (T.textEntryMake "")
 
 
 filterC :: Filter -> Doc FilterEvent Filter [H.Element]
 filterC = handle _EditorEvent
-          (\_ a -> L.set (fAv.aAv.R.chosen)
-                         (L.view (fTS.L._1.T.tText) a) a)
+          (\_ a -> L.set (fAvailable.R.chosen)
+                         (L.view (fTextSelect.L._1.T.tText) a) a)
           . handle _FilterEvent
-          (\_ a -> L.set (fTS.L._2)
-                         (selectFromAvailable (L.view fFi a)
-                                                (L.view fAv a)) a)
+          (\_ a -> L.set (fTextSelect.L._2)
+                         (selectFromAvailable (L.view fFilter a)
+                                                (L.view fAvailable a)) a)
           . handle (_SelectEvent.S.cEv)
-          (\i a -> L.over (fAv.aAv) (R.chooseIndex i) a)
+          (\i a -> L.over fAvailable (R.chooseIndex i) a)
 
           . filterA
 
