44c6c91278313e742673c92ef585f47eac8512ce
Tom Ellis <tom-2013@purelyagile.com>
2015-02-21 15:06:26 +0000

    Add textEntry

--- WX.hs
+++ WX.hs
@@ -1,11 +1,11 @@
 module Main where
 
-import Graphics.UI.WX hiding (Layout, Button, button, widget)
+import Graphics.UI.WX hiding (Layout, Button, button, widget, textEntry)
 import qualified Graphics.UI.WX as WX
 import Data.IORef
 import qualified Doc                as D
 import           Doc3               (DocF(Doc), DocP(DocP), DocR,
-                                     ReadMessage(ReadMessage))
+                                     ReadMessage(ReadMessage), contains, also)
 import qualified Doc3               as D3
 import           Control.Monad      (guard)
 import qualified Control.Monad.Trans.Writer as W
@@ -15,6 +15,8 @@ import qualified Radio              as R
 import qualified Data.Text.Lazy     as T
 
 data Button = Button String
+
+data TextEntry = TextEntry String
 
 buttonD :: Button -> D.Doc Layout ()
 buttonD (Button t) = D.Doc $ do
@@ -33,6 +35,22 @@ button b = (Doc
             . D.unDoc
             . buttonD) b
 
+textEntryD :: TextEntry -> D.Doc Layout ()
+textEntryD (TextEntry t) = D.Doc $ do
+  n <- D.unique
+  return (TextEntryL t n, parseMessage n)
+  where parseMessage n message = guard (message == n)
+
+textEntry :: TextEntry -> DocR () TextEntry Layout
+textEntry te = (Doc . DocP
+                . fmap (\(d, m) -> (ReadMessage (\message -> case m message of
+                                                    Nothing -> return te
+                                                    Just () -> do
+                                                      W.tell (DM.First (Just ()))
+                                                      return te), d))
+            . D.unDoc
+            . textEntryD) te
+
 list :: (s -> DocR e s l) -> NEL.NonEmpty s
      -> DocR e (NEL.NonEmpty s) (NEL.NonEmpty l)
 list w = R.sequenceNEL2 D3.pair D3.mapBD . fmap w
@@ -46,8 +64,13 @@ hello
 
        handler <- newIORef (\_ -> close f)
 
-       let widget  = fmap (D3.mapDoc (Row 1 . NEL.toList)) (list button)
-           initial = Button "Hello" NEL.:| [Button "Something", Button "Else"]
+       let widget = \(x1, y1) ->
+             ((,), \x y -> Column 1 [x, y])
+             `contains` (fmap (D3.mapDoc (Row 1 . NEL.toList)) (list button) x1)
+             `also` (fmap (D3.mapDoc (Row 1 . NEL.toList)) (list textEntry) y1)
+
+           initial = (Button "Hello" NEL.:| [Button "Something", Button "Else"],
+                      TextEntry "Foo" NEL.:| [])
 
        let loop state = do
              let D3.Doc (D3.DocP u) = widget state
@@ -66,6 +89,7 @@ hello
        return ()
 
 data Layout = ButtonL String T.Text
+            | TextEntryL String T.Text
             | Row Int [Layout]
             | Column Int [Layout]
 
@@ -75,6 +99,13 @@ renderLayout handlerRef f (ButtonL t ht) = do
                        handler <- readIORef handlerRef
                        handler ht
                    ]
+  return (set b [visible := False], WX.widget b)
+
+renderLayout handlerRef f (TextEntryL t ht) = do
+  b <- WX.textEntry f [text := t, on command := do
+                          handler <- readIORef handlerRef
+                          handler ht
+                      ]
   return (set b [visible := False], WX.widget b)
 
 renderLayout handlerRef f (Row i l) = do
